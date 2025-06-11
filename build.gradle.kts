3c1ca522c310007c59b4f1e8a420c0621500c6a5
jhon3242 <rldotnr2@gmail.com>
2025-06-11 19:43:06 +0900

    fix: 서브모듈 업데이트 테스크 분리

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,14 +88,13 @@ tasks.register("copyHooks") {
 }
 
 tasks.register<Copy>("copySecret") {
-    dependsOn(tasks.named("updateGitSubmodules"))
     from("./Yapp-26th-env")
     include("*.yml")
     into("src/main/resources")
     println("Secret files이 성공적으로 복사되었습니다.")
 }
 
-tasks.register<Exec>("updateGitSubmodules") {
+tasks.register<Exec>("initSubmodule") {
     group = "git"
     description = "Git submodule을 원격 저장소의 최신 버전으로 업데이트합니다."
     commandLine("git", "submodule", "update", "--remote")
