bbdd169b073047952578259bd7411cbc04ac7621
jhon3242 <rldotnr2@gmail.com>
2025-06-12 11:59:24 +0900

    feat: 에러 응답 API 테스트 추가

--- src/test/kotlin/com/yapp/yapp/document/Tag.kt
+++ src/test/kotlin/com/yapp/yapp/document/Tag.kt
@@ -4,6 +4,7 @@ enum class Tag(
     private val displayName: String,
 ) {
     MEMBER_API("Member API"),
+    ERROR_API("Error API"),
     ;
 
     fun getDisplayName(): String {
---
+++ src/test/kotlin/com/yapp/yapp/document/exception/ExceptionDocumentTest.kt
@@ -0,0 +1,39 @@
+package com.yapp.yapp.document.exception
+
+import com.yapp.yapp.common.exception.ErrorCode
+import com.yapp.yapp.document.Tag
+import com.yapp.yapp.document.support.BaseDocumentTest
+import io.restassured.RestAssured
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpHeaders
+import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
+
+class ExceptionDocumentTest : BaseDocumentTest() {
+    @Test
+    fun `커스텀 예외가 발생한다`() {
+        // given
+        val restDocsResponse =
+            response()
+                .responseBodyField(
+                    fieldWithPath("code").description("에러 코드"),
+                    fieldWithPath("message").description("에러 메시지"),
+                    fieldWithPath("timeStamp").description("응답 시간"),
+                )
+        val restDocsFilter =
+            filter("에러", "에러 응답")
+                .tag(Tag.ERROR_API)
+                .summary("에러 응답")
+                .description("에러를 응답 형식에 대해 설명합니다")
+                .response(restDocsResponse)
+                .build()
+
+        // when
+        // then
+        RestAssured.given(spec).log().all()
+            .filter(restDocsFilter)
+            .header(HttpHeaders.CONTENT_TYPE, "application/json")
+            .`when`().get("/exception")
+            .then().log().all()
+            .statusCode(ErrorCode.USER_NOT_FOUND.status)
+    }
+}
