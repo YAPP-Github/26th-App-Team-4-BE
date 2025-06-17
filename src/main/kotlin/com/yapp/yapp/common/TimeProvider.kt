38bec97af0f1f2ab81c1db36d1d5a1d222cfe652
jhon3242 <rldotnr2@gmail.com>
2025-06-17 19:00:32 +0900

    feat: Duration 클래스 java로 통일 및 요청시에는 String으로 변경

--- src/main/kotlin/com/yapp/yapp/common/TimeProvider.kt
+++ src/main/kotlin/com/yapp/yapp/common/TimeProvider.kt
@@ -1,12 +1,12 @@
 package com.yapp.yapp.common
 
-import java.time.Instant
 import java.time.OffsetDateTime
 import java.time.ZoneId
-import java.time.ZoneOffset
 
 object TimeProvider {
     fun now(): OffsetDateTime = OffsetDateTime.now(ZoneId.of("Asia/Seoul"))
 
-    fun parseByLong(dateTimeLon: Long): OffsetDateTime = Instant.ofEpochMilli(dateTimeLon).atOffset(ZoneOffset.UTC)
+    fun parse(timeStamp: String): OffsetDateTime {
+        return OffsetDateTime.parse(timeStamp)
+    }
 }
