9aaa981d9751593ff7296915be1fbc0fea636c7f
j-yong98 <chlwodyd9829@gmail.com>
2025-07-17 21:41:13 +0900

    FITRUN-146 fix: OidcProperties 역직렬화 문제

--- src/main/kotlin/com/yapp/yapp/common/token/oidc/OidcProperties.kt
+++ src/main/kotlin/com/yapp/yapp/common/token/oidc/OidcProperties.kt
@@ -1,12 +1,17 @@
 package com.yapp.yapp.common.token.oidc
 
-class OidcProperties(
-    val keySet: String,
-    val issuer: String,
-    val clientId: List<String>,
-) {
-    var nonce: String? = null
-        set(value) {
-            field = value
-        }
-}
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class OidcProperties
+    @JsonCreator
+    constructor(
+        @JsonProperty("keySet") val keySet: String,
+        @JsonProperty("issuer") val issuer: String,
+        @JsonProperty("clientId") val clientId: List<String>,
+    ) {
+        var nonce: String? = null
+            set(value) {
+                field = value
+            }
+    }
