8d17c3085c48fc32f4c709392da2b6ffeedf8c39
j-yong98 <chlwodyd9829@gmail.com>
2025-07-28 21:48:12 +0900

    FITRUN-72 test: 사용자 조회

--- src/test/kotlin/com/yapp/yapp/user/UserControllerTest.kt
+++ src/test/kotlin/com/yapp/yapp/user/UserControllerTest.kt
@@ -2,7 +2,7 @@ package com.yapp.yapp.user
 
 import com.yapp.yapp.common.web.ApiResponse
 import com.yapp.yapp.support.BaseControllerTest
-import com.yapp.yapp.user.api.response.UserResponse
+import com.yapp.yapp.user.api.response.UserAndGoalResponse
 import io.restassured.RestAssured
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Assertions.assertAll
@@ -10,10 +10,12 @@ import org.junit.jupiter.api.Test
 
 class UserControllerTest : BaseControllerTest() {
     @Test
-    fun `유저를 조회한다`() {
+    fun `목표가 있는 유저를 조회한다`() {
         // given
         val email = "test@test.com"
+        val user = userFixture.create(email)
         val accessToken = getAccessToken(email)
+        val userGoal = userGoalFixture.create(user)
 
         // when
         // then
@@ -26,12 +28,51 @@ class UserControllerTest : BaseControllerTest() {
                 .extract().`as`(ApiResponse::class.java)
                 .result
 
-        val response = objectMapper.convertValue(result, UserResponse::class.java)
+        val response = objectMapper.convertValue(result, UserAndGoalResponse::class.java)
 
         assertAll(
-            { Assertions.assertThat(response.userId).isNotNull() },
-            { Assertions.assertThat(response.email).isEqualTo(email) },
-            { Assertions.assertThat(response.nickname).isNotNull() },
+            { Assertions.assertThat(response.user.userId).isNotNull() },
+            { Assertions.assertThat(response.user.email).isEqualTo(email) },
+            { Assertions.assertThat(response.user.nickname).isNotNull() },
+            { Assertions.assertThat(response.user.provider).isNotNull() },
+            { Assertions.assertThat(response.user.runnerType).isNotNull() },
+            { Assertions.assertThat(response.goal!!.userId).isNotNull() },
+            { Assertions.assertThat(response.goal!!.goalId).isEqualTo(userGoal.id) },
+            { Assertions.assertThat(response.goal!!.paceGoal).isNotNull() },
+            { Assertions.assertThat(response.goal!!.timeGoal).isNotNull() },
+            { Assertions.assertThat(response.goal!!.runningPurpose).isNotNull() },
+            { Assertions.assertThat(response.goal!!.distanceMeterGoal).isNotNull() },
+            { Assertions.assertThat(response.goal!!.weeklyRunningCount).isNotNull() },
+        )
+    }
+
+    @Test
+    fun `목표가 없는 유저를 조회한다`() {
+        // given
+        val email = "test@test.com"
+        val user = userFixture.create(email = email)
+        val accessToken = getAccessToken(user.email)
+
+        // when
+        // then
+        val result =
+            RestAssured.given().log().all()
+                .header("Authorization", accessToken)
+                .`when`().get("/api/v1/users")
+                .then().log().all()
+                .statusCode(200)
+                .extract().`as`(ApiResponse::class.java)
+                .result
+
+        val response = objectMapper.convertValue(result, UserAndGoalResponse::class.java)
+
+        assertAll(
+            { Assertions.assertThat(response.user.userId).isNotNull() },
+            { Assertions.assertThat(response.user.email).isEqualTo(email) },
+            { Assertions.assertThat(response.user.nickname).isNotNull() },
+            { Assertions.assertThat(response.user.provider).isNotNull() },
+            { Assertions.assertThat(response.user.runnerType).isNotNull() },
+            { Assertions.assertThat(response.goal).isNull() },
         )
     }
 
