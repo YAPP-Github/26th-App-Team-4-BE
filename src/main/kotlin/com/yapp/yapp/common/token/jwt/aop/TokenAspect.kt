d9dff06a3d29e59be93c2406784cdc65cd464bcf
j-yong98 <chlwodyd9829@gmail.com>
2025-07-28 21:01:04 +0900

    FITRUN-167 fix: INVALID_REQUEST -> INTERNAL_SERVER 에러

--- src/main/kotlin/com/yapp/yapp/common/token/jwt/aop/TokenAspect.kt
+++ src/main/kotlin/com/yapp/yapp/common/token/jwt/aop/TokenAspect.kt
@@ -28,17 +28,22 @@ class TokenAspect(
     ): Any? {
         val attrs =
             RequestContextHolder.getRequestAttributes() as? ServletRequestAttributes
-                ?: throw CustomException(ErrorCode.INVALID_REQUEST)
+                ?: throw CustomException(ErrorCode.INTERNAL_SERVER)
 
         val request = attrs.request
-        val token =
+
+        val header =
             request.getHeader(AUTHORIZATION_HEADER)
-                ?.takeIf { it.startsWith(TOKEN_TYPE) }
-                ?.substring(TOKEN_TYPE.length)
                 ?: throw CustomException(ErrorCode.UNAUTHORIZED_REQUEST)
+
+        if (!header.startsWith(TOKEN_TYPE)) {
+            throw CustomException(ErrorCode.UNAUTHORIZED_REQUEST)
+        }
+
+        val token = header.removePrefix(TOKEN_TYPE)
         val tokenType = authenticated.tokenType
 
-        jwtTokenHandler.getUserId(token, tokenType)
+        jwtTokenHandler.validateToken(token, tokenType)
 
         return joinPoint.proceed()
     }
