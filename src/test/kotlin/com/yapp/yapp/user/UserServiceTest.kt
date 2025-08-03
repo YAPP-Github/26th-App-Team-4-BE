7a67cf6e795436b74d6569c524cbffd44804d0c3
j-yong98 <chlwodyd9829@gmail.com>
2025-08-03 16:57:40 +0900

    FITRUN-79 test: 회원 탈퇴 기능 테스트

--- src/test/kotlin/com/yapp/yapp/user/UserServiceTest.kt
+++ src/test/kotlin/com/yapp/yapp/user/UserServiceTest.kt
@@ -1,15 +1,27 @@
 package com.yapp.yapp.user
 
+import com.yapp.yapp.common.TimeProvider
+import com.yapp.yapp.record.domain.RecordsSearchType
+import com.yapp.yapp.record.domain.record.RunningRecordDao
 import com.yapp.yapp.support.BaseServiceTest
 import com.yapp.yapp.user.domain.RunnerType
+import com.yapp.yapp.user.domain.UserDao
 import com.yapp.yapp.user.domain.UserService
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.PageRequest
+import java.time.OffsetDateTime
 
 class UserServiceTest : BaseServiceTest() {
     @Autowired
     lateinit var userService: UserService
+
+    @Autowired
+    lateinit var userDao: UserDao
+
+    @Autowired
+    lateinit var runningRecordDao: RunningRecordDao
 
     @Test
     fun `러닝 기록이 없는 사용자의 추천 페이스는 기본 값이다`() {
@@ -36,5 +48,50 @@ class UserServiceTest : BaseServiceTest() {
 
         // then
         Assertions.assertThat(recommendPace.millsPerKm).isCloseTo(runnerType.recommendPace.millsPerKm, Assertions.within(1000L * 60))
+    }
+
+    @Test
+    fun `회원탈퇴 후 한달 뒤 회원 삭제한다`() {
+        // given
+        val user = userFixture.create(email = "test@test.com")
+        val deletedAt = TimeProvider.now().minusMonths(1)
+        runningFixture.createRunningRecord(user = user, totalSeconds = 60 * 20)
+        userFixture.createWithdrawUser(user = user, deletedAt = deletedAt)
+
+        // when
+        userService.cleanup()
+
+        Assertions.assertThat(userDao.findByEmail(user.email)).isNull()
+        Assertions.assertThat(
+            runningRecordDao.getRunningRecordList(
+                user = user,
+                targetDate = OffsetDateTime.now(),
+                type = RecordsSearchType.ALL,
+                pageable = PageRequest.of(1, 10),
+            ),
+        ).isEmpty()
+    }
+
+    @Test
+    fun `회원탈퇴 후 재가입한다`() {
+        // given
+        val user = userFixture.create(email = "test@test.com")
+        val deletedAt = TimeProvider.now().minusMonths(1)
+        userFixture.createWithdrawUser(user = user, deletedAt = deletedAt)
+        runningFixture.createRunningRecord(user = user, totalSeconds = 60 * 20)
+
+        // when
+        userService.cleanup()
+
+        Assertions.assertThat(userDao.findByEmail(user.email)).isNull()
+        Assertions.assertThat(
+            runningRecordDao.getRunningRecordList(
+                user = user,
+                targetDate = OffsetDateTime.now(),
+                type = RecordsSearchType.ALL,
+                pageable = PageRequest.of(1, 10),
+            ),
+        ).isEmpty()
+        Assertions.assertThat(userDao.save(user.email, user.nickname, user.provider)).isNotNull
     }
 }
